a397348e586bcf906dc112dd4c2ca72310c4f485
EmanuilDemianyuk <120477589+EmanuilDemianyuk@users.noreply.github.com>
2023-11-30 12:48:50 +0200

    fix type for context

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 import { TypeContext } from "src/Type";
 
-export const ThemeContext = createContext<TypeContext | null>(null);
+export const ThemeContext = createContext<any>(null);
 
